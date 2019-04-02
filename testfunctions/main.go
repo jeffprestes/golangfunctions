04d63eff5c727baf884aa851f9f4434fb219a2ca
Jeff Prestes <jeffprestes@gmail.com>
2019-04-02 08:27:46 -0300

    Add init example

--- testfunctions/main.go
+++ testfunctions/main.go
@@ -5,6 +5,10 @@ import (
 	"net/http"
 )
 
+func init() {
+	fmt.Println("Coloque aqui algo que você quer inicializar antes da execução da Cloud Function")
+}
+
 //SouUmTeste retornar o IP do usuario
 func SouUmTeste(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, r.UserAgent()+" seu IP é "+r.RemoteAddr+" ?")
